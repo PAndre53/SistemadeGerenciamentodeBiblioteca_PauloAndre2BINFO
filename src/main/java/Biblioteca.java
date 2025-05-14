f88fec3cd74760ed22b6a832ac1243e05a717df0
2024105070035 <2024105070035@aqs-lab-c-28.ifro.local>
2025-05-14 15:26:55 -0400

    Adicionado nova classe: Biblioteca e problema com GerenciadorBiblioteca resolvido.

---
+++ src/main/java/Biblioteca.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+ class Biblioteca {
+      //ATRIBUTOS
+    private ArrayList<Livro> livros;
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Administrador> administradores;
+}
--- src/main/java/GerenciadorBiblioteca.java
+++ src/main/java/GerenciadorBiblioteca.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
 public class GerenciadorBiblioteca {
 
-    //ATRIBUTOS
-    private ArrayList<Livro> livros;
-    private ArrayList<Usuario> usuarios;
-    private ArrayList<Administrador> administradores;
 }
